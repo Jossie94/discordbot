d5f4a093715382fe25c5b8cd3546fa5e045c7cf7
Josefine <jossansegel@gmail.com>
2021-09-07 12:12:27 +0200

    Creating basic commands  Register commands  Run it once

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection} = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('fs');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles)
+{
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
 });
 
+client.on('interactionCreate', async  interaction => {
+    if(!interaction.isCommand()) return;
+
+    const {commandName} = interaction;
+
+    if(commandName === 'ping')
+    {
+        await interaction.reply('Pong!');
+    }
+    else if(commandName === 'beep')
+    {
+        await interaction.reply('Boop!');
+    }
+   /* else if (commandName === 'server')
+    {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    }
+    else if (commandName === 'user')
+    {
+     await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    }*/
+});
 // Login to Discord with your client's token
 client.login(token);
