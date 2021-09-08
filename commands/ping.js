f3ed3fa0b3556c688423447cec4dc5d6ec6e1a6b
Senator Haax <filurt@hotmail.com>
2021-09-08 09:25:39 +0200

    DIS-12 Purge Functionality mvp purged functionality delete between 1 and 100 messages

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,6 @@ module.exports = {
         .setDescription('Replies with Pong'),
     async execute(interaction)
     {
-        console.log(interaction)
         await interaction.reply('Pong!');
     }
 }
