b7faa19e6e8b20d25212ac202c82a2c34b9b9a99
Senator Haax <filurt@hotmail.com>
2021-09-08 08:39:15 +0200

    DIS-12 Purge Functionality mvp purge command for testing try 2

--- commands/purge.js
+++ commands/purge.js
@@ -4,15 +4,19 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('purge')
         .setDescription('purges a given number of chat messeges'),
-    async execute(interaction,message,args)
+    async execute(interaction,message)
     {
-        var purgeamnt = args[0];
-        var purgelimit = Number(purgeamnt) + 1;
-        message.channel.messages.fetch({ limit: purgelimit }).then(messages => {
-            message.channel.bulkDelete(messages);
-            message.reply("deleted " + messages.array().length + " messages, including deletion command.");
-        }).catch(err => {
-            message.channel.send("Failed to delete messages. This may be caused by attempting to delete messages that are over 2 weeks old.");
-        });
+        const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+        const amount = args.join(' '); // Amount of messages which should be deleted
+
+        if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
+        if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
+
+        if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
+        if (amount < 1) return msg.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
+
+        await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
+            msg.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
+            )});
     },
 };
