365576c74392666c6363920c61848e2b7360be76
Jesper <jesperohansen@gmail.com>
2021-09-09 10:50:09 +0200

    DIS-4 Create GUI for MVP (Minimal design) Working on oAuth
    Require fetch won't work, tested all sorts of settings, followed the guide on oAuth, but somehow it won't work.
    
    Removed commented code fragment
    trying to see if we can run the bot on app.js and the oAuth in index.js

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-// Require the necessary discord.js classes
-const {Client, Intents, Collection} = require('discord.js');
-const {token} = require('./config.json');
-const fs = require('fs');
+const { port, clientId, clientSecret, url } = require('./config.json');
+const express = require('express');
+const fetch = require("node-fetch");
+const app = express();
 
-// Create a new client instance
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+app.get('/', async ({ query }, response) => {
+	const { code } = query;
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+	if (code) {
+		try {
+			const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientId,
+					client_secret: clientSecret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `http://${url}:${port}`,
+					scope: 'identify',
+				}),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
+			const oauthData = await oauthResult.json();
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+			const userResult = await fetch('https://discord.com/api/users/@me', {
+				headers: {
+					authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+				},
+			});
 
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+			console.log(await userResult.json());
+		} catch (error) {
+			// NOTE: An unauthorized token will not throw an error;
+			// it will return a 401 Unauthorized response in the try block above
+			console.error(error);
+		}
 	}
-}
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		return interaction.reply({content: 'There was an error while executing this command', ephemeral: true});
-	}
+	// Send to index.html looking in root directory
+	return response.sendFile('index.html', { root: '.' });
 });
-
-// Login to Discord with your client's token
-client.login(token);
+app.listen(port, () => console.log(`App listening at http://${url}:${port}`));
