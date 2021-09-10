5b3325129d40ea8dceaa0d6e5206669d5d669294
Josefine <jossansegel@gmail.com>
2021-09-10 07:44:55 +0200

    Creating basic commands  Register commands  Run it once

--- commands/annoucement.js
+++ commands/annoucement.js
@@ -11,6 +11,7 @@ module.exports = {
     {
         await interaction.reply('Today is a good day');
     }
-}
+
+    }
 
 //Automatically @everyone
--- commands/beep.js
+++ commands/beep.js
@@ -4,7 +4,8 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('beep')
         .setDescription('Replies with boop'),
-    async execute(interaction) {
+    async execute(interaction)
+    {
         await interaction.reply('Boop!');
     },
 };
