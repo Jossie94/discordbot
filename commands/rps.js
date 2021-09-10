708c7e0efb65fd01545711cd072210de13a52ad5
Senator Haax <filurt@hotmail.com>
2021-09-10 12:41:07 +0200

    DIS-14 Game functionality rock paper scissors lizard spock mvp (regular rock paper scissors)

--- commands/rps.js
+++ commands/rps.js
@@ -24,7 +24,7 @@ module.exports = {
         // const args = message.content
         const playerChoice = elements.find((obj) => { if (obj.value === interaction.options.getString('string').toLowerCase()) return obj; return null});
         console.log(interaction.options.getString('string').toLowerCase());
-        if (interaction.options.getString('string').toLowerCase() !== 'rock'||interaction.options.getString('string').toLowerCase() !== 'paper'||interaction.options.getString('string').toLowerCase() !== 'scissor'||interaction.options.getString('string').toLowerCase() !== 'scissors'||interaction.options.getString('string').toLowerCase() !== 'haax') {
+        if (interaction.options.getString('string').toLowerCase() !== 'rock'||interaction.options.getString('string').toLowerCase() !== 'paper'||interaction.options.getString('string').toLowerCase() !== 'scissor'||interaction.options.getString('string').toLowerCase() !== 'scissors') {
             return interaction.reply('you done fucked upppppppp and choose a non existent option: '+interaction.options.getString('string').toLowerCase())
         }
         const botChoice = elements[Math.floor(Math.random() * 2)];
