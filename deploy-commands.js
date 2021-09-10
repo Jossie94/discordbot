7d572874f5322fda27ba02d15d211c32ba775b52
Senator Haax <filurt@hotmail.com>
2021-09-10 13:29:54 +0200

    master formatiing every file so far

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,29 +1,27 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const {REST} = require('@discordjs/rest');
-const {Routes} = require ('discord-api-types/v9');
-const { clientId, guildId, token} = require('./config.json');
+const {Routes} = require('discord-api-types/v9');
+const {clientId, guildId, token} = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles)
-{
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({version: '9' }).setToken(token);
+const rest = new REST({version: '9'}).setToken(token);
 
-(async  () => {
+(async () => {
     try {
-        await  rest.put(
+        await rest.put(
             Routes.applicationGuildCommands(clientId, guildId),
             {body: commands},
         );
         console.log('Successfully registered application commands.');
-    } catch (error)
-    {
+    } catch (error) {
         console.error(error);
     }
 })();
