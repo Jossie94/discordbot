02f372b48c1a99da9938cb3f3820b747f4a929ce
Senator Haax <filurt@hotmail.com>
2021-09-22 14:48:23 +0200

    end of day leaderboard is currently only missing convertind discord id to discord usernames

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -17,16 +17,16 @@ module.exports = {
         con.query(`SELECT points, u_token
                               FROM leaderboard
                               WHERE u_server = ? ORDER BY points LIMIT 0,10 `, [`${interaction.guild.id}`], function (err, result) {
-                return interaction.reply("first place:"+result[0].u_token +
-                    "\n2nd place:"+result[1].u_token +
-                    "\n3rd place:"+result[2].u_token +
-                    "\n4th place:"+result[3].u_token +
-                    "\n5th place:"+result[4].u_token +
-                    "\n6th place:"+result[5].u_token +
-                    "\n7th place:"+result[6].u_token +
-                    "\n8th place:"+result[7].u_token +
-                    "\n9th place:"+result[8].u_token +
-                    "\n10th place:"+result[9].u_token
+                return interaction.reply("1st place:"+result[0].u_token +' points:'+result[0].points+
+                    "\n2nd place:"+result[1].u_token +' points:'+result[1].points+
+                    "\n3rd place:"+result[2].u_token +' points:'+result[2].points+
+                    "\n4th place:"+result[3].u_token +' points:'+result[3].points+
+                    "\n5th place:"+result[4].u_token +' points:'+result[4].points+
+                    "\n6th place:"+result[5].u_token +' points:'+result[5].points+
+                    "\n7th place:"+result[6].u_token +' points:'+result[6].points+
+                    "\n8th place:"+result[7].u_token +' points:'+result[7].points+
+                    "\n9th place:"+result[8].u_token +' points:'+result[8].points+
+                    "\n10th place:"+result[9].u_token+' points:'+result[9].points
                 );
         })
     },
