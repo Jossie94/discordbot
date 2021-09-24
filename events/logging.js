b9b09ca6c46370b651202000dc78c43b447bdb56
Senator Haax <filurt@hotmail.com>
2021-09-24 13:23:00 +0200

    minor code cleanup fixed spelling mistakes and renamed filed to make more sense

--- events/logging.js
+++ events/logging.js
@@ -3,7 +3,7 @@ const Utils = require('../utils/usefull_functions');
 module.exports = {
     name: 'interactionCreate',
     execute(interaction) {
-        Utils.upsertUser(interaction.user, interaction.guild.id);//create or update caster in the database
+        Utils.upsertUser(interaction.user, interaction.guild.id);
         switch (interaction.commandName) {
             case 'purge':
                 let description = (interaction.options.getString('description') === null) ? 'No description' : interaction.options.getString('description')
@@ -12,7 +12,7 @@ module.exports = {
             case 'role':
                 let options = interaction.options;
                 let role = options._hoistedOptions[0].value;
-                Utils.upsertUser(options._hoistedOptions[1].user, interaction.guild.id);//creates or updates target of command
+                Utils.upsertUser(options._hoistedOptions[1].user, interaction.guild.id);
                 Utils.log(`${interaction.user.username} Tried to give role: ${role} to user: ${options._hoistedOptions[1].user.username}`,interaction.user.id,options._hoistedOptions[1].user.id);
                 break;
         }
