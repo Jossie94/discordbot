474fdd88aef5f271c6897373740e3e88a7743611
Senator Haax <filurt@hotmail.com>
2021-09-24 14:22:36 +0200

    added more precense to my bot

--- events/inital_login.js
+++ events/inital_login.js
@@ -3,6 +3,6 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        client.user.setPresence({ activities: [{ name: 'reaking havoc' }], status: 'online' });
+        client.user.setPresence({ activities: [{ name: 'reaking havoc'},{ name: 'testing the havoc i reaked'},], status: 'online' });
     },
 };
