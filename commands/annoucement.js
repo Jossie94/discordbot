9340f9d5e0dbdf872504b90af7bb244f37947a76
Josefine-1 <Josefine-1@VISTI>
2021-10-04 18:22:36 +0200

    reply interaction home

--- commands/annoucement.js
+++ commands/annoucement.js
@@ -1,17 +1,15 @@
-const Utils = require('../utils/usefull_functions');
-const {SlashCommandBuilder} = require('@discordjs/builders');
+// const Utils = require('../utils/usefull_functions');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('announce')
-        .setDescription('Announcement').addStringOption(option =>
-            option.setName('string')
-                .setDescription('which option to choose')
-                .setRequired(true).addChoice("rock", "rock").addChoice("paper", "paper").addChoice("scissors", "scissors")),
-    async execute(interaction) {
-const message = interaction.options.getString('input');
-
-
-
-    },
+	data: new SlashCommandBuilder()
+		.setName('announce')
+		.setDescription('Announcement').addStringOption(option =>
+			option.setName('string')
+				.setDescription('write your message')
+				.setRequired(true)),
+	async execute(interaction) {
+		const message = interaction.options.getString('input');
+		await interaction.reply(message);
+	},
 };
