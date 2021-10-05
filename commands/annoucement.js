5c50bda7a59a6d8d4b1490d7f528320416b05f99
Josefine <jossansegel@gmail.com>
2021-10-05 14:39:52 +0200

    DIS-17 Timed announcement announce in different channels

--- commands/annoucement.js
+++ commands/annoucement.js
@@ -1,15 +1,23 @@
-// const Utils = require('../utils/usefull_functions');
+ const Utils = require('../utils/usefull_functions');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('announce')
 		.setDescription('Announcement').addStringOption(option =>
-			option.setName('string')
+			option.setName('message')
 				.setDescription('write your message')
-				.setRequired(true)),
+				.setRequired(true)).addChannelOption(option =>
+		option.setName('channel')
+			.setDescription('type in channel')
+			.setRequired(true)),
 	async execute(interaction) {
-		const message = interaction.options.getString('input');
-		await interaction.reply(message);
+				const message = interaction.options.getString('message');
+				const channel = interaction.options.getChannel('channel');
+				await interaction.reply('Sent to channel '+ channel.name);
+				channel.send(message);
+
+		//let channel = interaction.channel.getString('input');
+
 	},
 };
