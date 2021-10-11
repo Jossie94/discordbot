0d71c93e66f5de4003442b44921b76cb324048eb
Josefine <jossansegel@gmail.com>
2021-10-11 07:55:59 +0200

    finalized timeannocement

--- commands/timeannoucement.js
+++ commands/timeannoucement.js
@@ -9,17 +9,23 @@ module.exports = {
         .setDescription('User types in announcement message').addStringOption(option =>
             option.setName('message')
                 .setDescription('the message')
-                .setRequired(true))/*.addIntegerOption(options =>
+                .setRequired(true)).addIntegerOption(options =>
             options.setName('hours')
                 .setDescription('choose time')
-                .setRequired(true))*/,
+                .setRequired(true)).addChannelOption(options =>
+        options.setName('channel')
+            .setDescription('Choose channel')
+            .setRequired(true)),
+
     async execute(interaction) {
 
         const userinput = interaction.options.getString('message');
-        //let inputhours = interaction.options.getInteger('hours');
-        await Utils.wait(1);
-        //const channelinput =
-        interaction.reply(userinput);
+        const inputhours = interaction.options.getInteger('hours');
+        const channelinput = interaction.options.getChannel('channel');
+        interaction.reply('Sent commando');
+        await Utils.wait(inputhours);
+        channelinput.send(userinput);
+
     },
 };
 
