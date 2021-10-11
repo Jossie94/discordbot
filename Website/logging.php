0cd241f417c4ae2ed18147b68daf52246e5f64c0
Senator Haax <filurt@hotmail.com>
2021-10-11 08:00:59 +0200

    DIS-20 Advanced logging Finalized mvp log view for presentation

--- Website/logging.php
+++ Website/logging.php
@@ -1,5 +1,7 @@
 <?php
 $isLoggedIn = false;
+$id = '';//todo add user id on login
+//$sql = mysqli_query(mysqli_connect("projectfritid.com", "Skole", "Skole123", "discordbot"), "SELECT is_dev FROM user WHERE id = {$id}");
 ?>
 <html>
 <head>
@@ -14,15 +16,14 @@ $isLoggedIn = false;
 <nav>
     <ul>
         <li><a href="index.php">RPS Leaderboard</a></li>
-        <?php if ($isLoggedIn){?>
+        <?php if ($isLoggedIn) { ?>
             <li><a href="logging.php">Logging</a></li>
         <?php } ?>
         <li><a href="login.php">Login</a></li>
     </ul>
 </nav>
 <section>
-    <table>
-        <!--  DO WORK HERE  -->
+    <table style="border-spacing: 4px; background-color: lightgreen; float: right">
         <tr>
             <th>ID</th>
             <th>Command</th>
@@ -32,21 +33,41 @@ $isLoggedIn = false;
         </tr>
         <?php
         // SQL query to interact with info from our database
-        $sql = mysqli_query(mysqli_connect("projectfritid.com","Skole","Skole123","discordbot"),"SELECT * FROM log ORDER BY id DESC LIMIT 10");
+        $styling = '';
+        $sql = mysqli_query(mysqli_connect("projectfritid.com", "Skole", "Skole123", "discordbot"), "SELECT * FROM log ORDER BY id LIMIT 10");
         $i = 0;
         // Establish the output variable
 
-        while($row = mysqli_fetch_array($sql)){
+        while ($row = mysqli_fetch_array($sql)) {
+            $styling = ($i % 2) ? 'background-color: darkgrey;' : 'background-color: grey;';
+
             echo "<tr>";
-            echo "<td>".$row['id']."</td>";
-            echo "<td>".$row['command']."</td>";
-            echo "<td>".$row['caster']."</td>";
-            echo "<td>".$row['target']."</td>";
-            echo "<td>".$row['timestamp']."</td>";
+            echo "<td style='padding: 5px; {$styling}'>" . $row['id'] . "</td>";
+            echo "<td style='padding: 5px; {$styling}'>" . $row['command'] . "</td>";
+            echo "<td style='padding: 5px; {$styling}'>" . $row['caster'] . "</td>";
+            echo "<td style='padding: 5px; {$styling}'>" . $row['target'] . "</td>";
+            echo "<td style='padding: 5px; {$styling}'>" . $row['timestamp'] . "</td>";
             echo "</tr>";
+            $i++;
         }
         ?>
     </table>
+    <div style="margin: 10px; float: left; background-color: lightgray; width: 250px; height: 350px;">
+        <form style="display: table-caption" action="logging.php">
+            <label for="caster" style="margin-left: 95px;">Caster
+                <input id="caster" type="text" style="margin-left: 25px;">
+            </label>
+            <div style="margin:15px;"></div>
+            <label for="target" style="margin-left: 95px;">Target
+                <input id="target" type="text" style="margin-left: 25px;">
+            </label>
+            <div style="margin:15px;"></div>
+            <label for="message" style="margin-left: 95px;">Message
+                <input id="message" type="text" style="margin-left: 25px;">
+            </label>
+            <input type="submit" value="submit" style="margin-left: 90px;margin-top: 15px;">
+        </form>
+    </div>
 </section>
 </body>
 </html>
